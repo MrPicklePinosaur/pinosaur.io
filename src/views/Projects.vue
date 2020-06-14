efd5602d946b49d5e414e5df5fe97fb7b3e4c644
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-06-14 13:39:45 -0400

    basics

---
+++ src/views/Projects.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Projects extends Vue{
+    
+}
+</script>
+
+<style>
+
+</style>
