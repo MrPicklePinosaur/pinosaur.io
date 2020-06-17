7c8ea77b00913badfbbc6b081fbdba0a62f8414a
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-06-17 16:33:58 -0400

    custom search

---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const projectAssetPath = '@/assets/projects/';
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,8 +1,30 @@
 <template>
   <div>
+    <p v-text="searchBarContents"></p>
+    <!-- <div>
+      <input
+        class="search-bar"
+        autocomplete="off"
+        spellcheck="off"
+        placeholder="search projects"
+        v-model="searchBarContents"
+      >
+    </div> -->
+
+    <div class="projectSearch-main">
+      <div class="projectSearch-tags">
+
+      </div>
+
+      <input
+        class="projectSearch-field"  
+        autocomplete="off"
+        spellcheck="false"
+        v-model="searchBarContents"
+      >
+    </div>
 
     <el-tag effect="dark"> 
-      
       <el-button type="primary">x</el-button>
       Tag 
     </el-tag>
@@ -15,6 +37,7 @@
 import { Component, Vue } from 'vue-property-decorator'
 import { projectList } from '@/data/projects'
 import Project from '@/components/Project.vue'
+import { ProjectInfo } from '@/types'
 
 @Component({
   components: {
@@ -23,11 +46,48 @@ import Project from '@/components/Project.vue'
 })
 export default class Projects extends Vue{
 
-  projectList = projectList;
+  projectList: ProjectInfo[] = [];
 
+  searchBarContents = '';
+
+  created() {
+    this.projectList = projectList;
+  }
 }
 </script>
 
 <style>
+
+  .projectSearch-main {
+    display: flex;
+    padding: 0;
+    margin: 0;
+    height: 2rem;
+
+    border: 2px solid black;
+    border-radius: 4px;
+
+  }
+
+  .projectSearch-tags {
+    padding: 0;
+    margin: 0;
+
+  }
+
+  .projectSearch-field {
+    height: 100%;
+
+    border: none;
+    outline: none;
+
+    padding: 0;
+    margin: 0;
+
+    box-shadow: none;
+    resize: none;
+    overflow: auto; 
+    white-space: nowrap;
+  }
 
 </style>
